59be2184cf3873fb84ea68ee1cabec3b4972edb7
Victoria Raya <victoriaraya111@gmail.com>
2024-10-02 22:34:31 -0700

    faq edits

---
+++ app/faq/page.tsx
@@ -0,0 +1,18 @@
+const Faq = () => {
+  return (
+    <div>
+      <p>Are there fees for your services? Do you take insurance?</p>
+      <p>
+        Yes there is a charge. Unfortunately, end-of-life doula services are not
+        converd by Medicare or other insurance. Instead, such services, like
+        other companion/caregiver type services are typically paid for by
+        individuals and families as an out-of-pocket private expense. There are
+        discounts available if you choose multiple services. Payment plans are
+        available as well. I can work with all budgets. Please contact me today
+        to schedule a free consultation.
+      </p>
+    </div>
+  );
+};
+
+export default Faq;
