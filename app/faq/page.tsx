fdde9976347df6ee8d2eb22230ee7252375430eb
Victoria Raya <victoriaraya111@gmail.com>
2024-10-04 20:41:52 -0700

    faq edits

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -11,6 +11,21 @@ const Faq = () => {
         available as well. I can work with all budgets. Please contact me today
         to schedule a free consultation.
       </p>
+      <p>
+        What is the difference between a hospice volunteer and an end-of-life
+        doula?
+      </p>
+      <p>
+        The death doula's service do not take the place of hospice personnel;
+        rather they complement other services that a dying person and their
+        family may be receiving buy providing additional support and community
+        resources for end of life care for the patient and their families. There
+        are laws governing what death doulas can and cannot do. <br />
+        Hospice volunteers are mandated by Medicare law and the hospice care
+        providers' hours are regulated by Medicare. Medicare law requiers that
+        volunteer hours equal at least five percent of the hospice provider's
+        total patient care hours.
+      </p>
     </div>
   );
 };
