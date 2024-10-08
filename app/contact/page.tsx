5e0b5fc8a42e95c2d0817361f80ccaddc74efda5
Victoria Raya <victoriaraya111@gmail.com>
2024-10-07 23:17:37 -0700

    add contact and resources page

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      email me at ...
+      <p>form here </p>
+    </div>
+  );
+};
+
+export const Contact;
---
+++ app/resources/page.tsx
@@ -0,0 +1,10 @@
+const Resources = () => {
+  return (
+    <div>
+      mortuary, caregiving, hospice agency, notary, boarding care facilities,
+      education, community outreach resources, hippa
+    </div>
+  );
+};
+
+export default Resources;
