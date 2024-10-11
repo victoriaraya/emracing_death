970ee716bc7073ba6f057f830c52db5ee7493906
Victoria Raya <victoriaraya111@gmail.com>
2024-10-11 15:30:26 -0700

    home page and navbar edits

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,78 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full h-full bg-[#4F7641]">
-      <div>
+      <div className="relative w-full h-screen">
         <Image
-          src={"/images/flower.jpg"}
+          src={"/images/flower_cropped2.jpg"}
           alt="flower"
           width={550}
           height={400}
           quality={100}
-          className="relative top-0 left-0 w-full h-screen"
+          className="absolute top-0 left-0 w-full h-screen"
         />
-        <h1 className="absolute inset-0 flex flex-col text-center items-center justify-center text-7xl">
+        <h1 className="absolute inset-0 flex flex-col text-center items-center justify-center px-3 md:text-7xl text-4xl text-[#341C09]">
           Family and patient centered support for end-of-life transitions
         </h1>
       </div>
-      <div className="h-screen"> Why a death doula </div>
+      <div className="h-full flex-col items-center justify-items-center text-left px-28 py-16 text-[#CDBA9D]">
+        <h1 className="text-4xl pb-2">What is an End-of-Life Doula?</h1>
+        <p className="text-xl">
+          A Death Doula/End-of-Life Doula is a non-medical, trained professional
+          who provides support via education, organization, planning and
+          resources for end of life similar to the aide a birth doula provides
+          during pregnancy and through the birthing process. <br />
+          The role of a Death Doula is t oassist families prior to, throughout
+          and after the dying process. This assistance may include, but is not
+          limited to, helping families create plans and have conversations prior
+          to a person entering the end-of-life stages, creating a cofort and
+          care plan for end-of-life, assigning a person to carry out healthcare
+          decisions, estate planning and legacy projects. <br />
+          We can work alongside a hospice and palliative care team, but we offer
+          a separate non medical service. <br />
+          A few other things death doulas can help with is providing spiritual,
+          emotional and social support before and just after death. <br />
+          Death doulas provide emotional support, assist with decision-making
+          and help create a peaceful and meaningful end-of-life experience. By
+          offering compassionate care that is tailored to the individual's
+          values and preferences, death doulas can help individuals and their
+          families navigate the complexities of the dying process with dignity
+          and respect.
+        </p>
+      </div>
+      <div className="h-full flex-col items-center justify-items-center text-left px-28 py-16 bg-[#CDBA9D] text-[#4F7641]">
+        <h1 className="text-4xl pb-2">Benefits of an End-of-Life Planner:</h1>
+        <ul className="text-xl list-disc">
+          <li>
+            Facilitating end-of-life planning to help prepare practicalities,
+            ease anxiety, aid in comfort and promote a personalized passage.{" "}
+          </li>
+          <li>
+            {" "}
+            Increasing quuality of life moments for clients and loved ones by
+            providing resources and support to client and loved ones through the
+            end-of-life journey.{" "}
+          </li>
+          <li>
+            {" "}
+            Supporting the patient and their loved ones with resources regarding
+            end-of-life arrangements.{" "}
+          </li>
+          <li>
+            {" "}
+            Providing with resources to bring closure to end of life planning,
+            ensure the support to have any tasks required performed for greater
+            peace of mind and to ease the transition.{" "}
+          </li>
+          <li>
+            Serving as support to the client and loved ones through the
+            end-of-life journey.{" "}
+          </li>
+          <li>
+            Increasing quality of life moments for clients and loved ones as the
+            end-of-life planner serves as a resource to facilitate.{" "}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,6 +2,14 @@
 
 import { useState } from "react";
 import Link from "next/link";
+import { Noto_Sans_Ethiopic } from "next/font/google";
+
+const notoSansEthiopic = Noto_Sans_Ethiopic({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-notoSansEthiopic",
+  weight: "200",
+});
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +19,9 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="relative text-[#8C4A18] bg-[#CDBA9D] h-32">
+    <nav
+      className={`${notoSansEthiopic.className} relative text-[#CDBA9D] bg-[#2c393a] md:h-32 h-16`}
+    >
       <div className="flex justify-center items-center">
         <button
           className="text-3xl md:hidden focus:outline-none z-40"
@@ -22,39 +32,39 @@ const NavBar = () => {
       </div>
 
       <ul
-        className={`flex flex-col md:flex-row gap-3 sm:gap-8 justify-center items-center text-center text-2xl font-bold w-full absolute top-0 left-0 mt-14 md:mt-0 sm:p-3 z-20 ${
+        className={`flex flex-col md:flex-row gap-3 sm:gap-8 justify-center items-center text-center text-2xl font-bold w-full sm:h-full absolute top-0 left-0 mt-14 md:mt-0 sm:p-3 z-20 ${
           isOpen ? "block" : "hidden"
-        } md:flex bg-[#CDBA9D]`}
+        } md:flex bg-[#2c393a]`}
       >
-        <li className="relative group">
-          <Link href="/" onClick={() => setIsOpen(false)}>
-            Meaningful EndCare
-          </Link>
-          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#8C4A18] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
-        </li>
         <li className="relative group">
           <Link href="/faq" onClick={() => setIsOpen(false)}>
             FAQ
           </Link>
-          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#8C4A18] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#CDBA9D] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
         </li>
         <li className="relative group">
           <Link href="/services" onClick={() => setIsOpen(false)}>
             Services
           </Link>
-          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#8C4A18] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#CDBA9D] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+        </li>
+        <li className="relative group">
+          <Link href="/" onClick={() => setIsOpen(false)}>
+            Meaningful EndCare
+          </Link>
+          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#CDBA9D] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
         </li>
         <li className="relative group">
           <Link href="/about" onClick={() => setIsOpen(false)}>
             About
           </Link>
-          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#8C4A18] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#CDBA9D] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
         </li>
-        <li className="relative group">
+        <li className="relative group sm:pb-0 pb-2">
           <Link href="/contact" onClick={() => setIsOpen(false)}>
             Contact
           </Link>
-          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#8C4A18] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+          <span className="absolute left-1/2 bottom-[-3px] w-0 h-[2px] bg-[#CDBA9D] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
         </li>
       </ul>
     </nav>
