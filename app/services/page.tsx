49b1b42b1a23ec9dd20e067a2c86e69a25ef9113
Victoria Raya <victoriaraya111@gmail.com>
2024-10-13 19:47:59 -0700

    services page style edits

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -3,8 +3,8 @@ import { ServicesList } from "@/components/ServicesDoc";
 
 const Services = () => {
   return (
-    <div className="flex flex-col gap-5">
-      <h1>Services</h1>
+    <div className="flex flex-col gap-5 bg-[#4F7641] text-[#CDBA9D] h-full px-6 pb-4">
+      <h1 className="text-5xl text-center py-4">Services</h1>
       {ServicesList.map((service, idx) => (
         <ServiceTile service={service} key={idx} />
       ))}
--- components/ServiceTile.tsx
+++ components/ServiceTile.tsx
@@ -2,7 +2,7 @@ const ServiceTile = (props) => {
   const service = props.service;
 
   return (
-    <div className="bg-slate-200">
+    <div className="bg-[#45260C]">
       <h1>{service.name}</h1>
       <p>{service.description}</p>
     </div>
