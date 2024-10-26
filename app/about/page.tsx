f04351dfd9ad8686ad8b139d4a53474923d9ce5e
Victoria Raya <victoriaraya111@gmail.com>
2024-10-25 22:00:38 -0700

    about edits

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,46 +11,33 @@ const About = () => {
           <div className="text-lg pb-32">
             <p>
               {" "}
-              I've no doubt you've heard Antoine de Saint-Exupery's quote, 'A
-              goal without a plan in just a wish.' The quote above empasizes the
+              'A goal without a plan in just a wish.' This quote empasizes the
               importance of having a clear plan to achieve a goal. By creating
-              end of life plans ahead, ensures that your wishes are respected,
-              providing clear guidance and reducing the potential for family
-              conflicts and alleviates the burden on the loved ones during an
-              emotionally challenging time. Making an end of life arrangement
-              plan helps prevents unexpected financial burdens on the loved
-              onesand knowing that you have a plan in place can bring peace of
-              mind to you and your family.{" "}
+              end-of-life plans, you ensure your wishes are respected, lessen
+              the emotional and financial burden on loved ones, and bring peace
+              of mind to you and your family.{" "}
             </p>
             <br />
             <p>
-              Many people have not thought about the arrangement of end of life
-              and feel overwhelmed about where to start the process. I hope to
-              help ease some of the most common questions and unsurety while
-              caring for a loved one who is going through the ding process.{" "}
+              Many people have not thought about end-of-life arrangements and
+              feel overwhelmed about where to begin. I'm here to help answer
+              common questions and provide support throughout the process.{" "}
             </p>
             <br />
             <p>
-              My work starts when people are healthy, I help them start to think
-              about wha t the end of their lives might look like and help them
-              create a plan for doing so; when someone is looking at the end of
-              their lives, meaning there's a terminal illness or they're getting
-              very elderly, I help them develop a personalized plan about what
-              would be a peaceful dying for them; after a death, I help families
-              wrap up the affairs of a life.
+              My work begins when people are healthy; I help them consider what
+              the end of their life might look like and support them in creating
+              a plan. For those facing terminal illness or advanced age, I
+              assist in developing a personalized plan for a peaceful
+              end-of-life experience. After a passing, I guide families in
+              wrapping up the affairs of a loved one's life.
             </p>{" "}
             <br />
             <p>
               The goal of Meaningful EndCare is to provide holistic,
               compassionate care that addresses the physical, emotional,
-              spiritual and practical needs of the dying person and their loved
+              spiritual, and practical needs of the dying person and their loved
               ones.{" "}
-            </p>
-            <br />
-            <p>
-              As a certified End of Life Death Doula, as a hospice nurse, I do
-              possess the personal strengths and skills that are valuable for
-              being an end-of-life doula.{" "}
             </p>
           </div>
         </div>
