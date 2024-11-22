d9b46b7a68360258eee732bccb29ef14148caf9d
Victoria Raya <victoriaraya111@gmail.com>
2024-11-21 17:13:31 -0800

    responsive services page for mobile

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -3,9 +3,11 @@ import { ServicesList } from "@/components/ServicesDoc";
 
 const Services = () => {
   return (
-    <div className="flex flex-col gap-5 [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#d2b48c_100%)] text-[#4F7641] h-full pb-4 px-20">
-      <h1 className="text-7xl text-center py-8">Services</h1>
-      <div className="grid grid-cols-3 gap-8 pb-14">
+    <div className="flex flex-col gap-5 [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#d2b48c_100%)] text-[#4F7641] h-full pb-4 px-5 sm:px-20">
+      <h1 className="text-5xl sm:text-7xl text-center py-8 self-center">
+        Services
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 pb-14">
         {ServicesList.map((service, idx) => (
           <ServiceTile service={service} key={idx} />
         ))}
@@ -13,7 +15,7 @@ const Services = () => {
       <h2 className="text-[#4F7641] text-center text-3xl pb-20">
         <a href="mailto:meaningfulendcare@gmail.com" className="relative group">
           Contact us to schedule a free consultation
-          <span className="absolute left-1/2 bottom-[-5px] w-0 h-[2px] bg-[#4F7641] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+          <span className="absolute left-1/2 bottom-[-5px] w-0 h-[2px] bg-[#4F7641] transition-all duration-300 sm:group-hover:w-full sm:group-hover:left-0"></span>
         </a>
       </h2>
     </div>
