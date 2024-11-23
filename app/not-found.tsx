5f1f85f5423888d19b47f73ed2b662f1c1c7eb08
Victoria Raya <victoriaraya111@gmail.com>
2024-11-23 13:21:31 -0800

    responsive not found page for mobile

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="h-screen w-full text-[#4F7641] pt-28 flex flex-col text-2xl text-center [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#d2b48c_100%)] leading-loose">
+    <div className="h-screen w-full text-[#4F7641] pt-20 sm:pt-28 flex flex-col text-xl sm:text-2xl text-center [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#d2b48c_100%)] leading-loose sm:leading-loose">
       <p>We couldn't find the page you were looking for.</p>
-      <p className="pt-3 px-44">
+      <p className="pt-3 px-4 sm:px-44">
         There may be an error in the URL entered into your web browser. Please
         check the URL and try again.
       </p>
@@ -12,7 +12,7 @@ const NotFound = () => {
         You can return to the homepage by clicking{" "}
         <span className="relative group">
           <Link href="/">here.</Link>
-          <span className="absolute left-1/2 bottom-[-2px] w-0 h-[2px] bg-[#4F7641] transition-all duration-300 group-hover:w-full group-hover:left-0"></span>
+          <span className="absolute left-1/2 bottom-[-2px] w-0 h-[2px] bg-[#4F7641] transition-all duration-300 sm:group-hover:w-full sm:group-hover:left-0"></span>
         </span>
       </p>
     </div>
